c18e6dcde218c81eae1dff8e00177356388813d1
Grigoriy Dobryakov <dobryakov@playform.com>
2012-10-23 16:48:29 +0400

    add more comments to config

--- resources/fotostrana.config.php
+++ resources/fotostrana.config.php
@@ -1,23 +1,31 @@
 <?php
 
-define('FOTOSTRANA_APPID', 'sdkdemo');
-define('FOTOSTRANA_CLIENTKEY', 'a84aedc74dda60a3cf35a96683ae0947');
-define('FOTOSTRANA_SERVERKEY', 'c95e03377ca085d6f82c9b39fbf9336e');
+// следующие параметры возьмите из настроек приложения (создайте его, если у вас его нет)
+define('FOTOSTRANA_APPID', 'sdkdemo'); // ваш APP_ID
+define('FOTOSTRANA_CLIENTKEY', 'a84aedc74dda60a3cf35a96683ae0947'); // клиентский ключ
+define('FOTOSTRANA_SERVERKEY', 'c95e03377ca085d6f82c9b39fbf9336e'); // серверный ключ
 
+// вы можете использовать тестовые адреса для отладки,
+// а для реального использования замените fs98.vs58.net на fotostrana.ru в обоих строках
 define('FOTOSTRANA_URL', 'http://fs98.vs58.net');
 define('FOTOSTRANA_API_BASEURL', 'http://fs98.vs58.net/apifs.php');
+
+// параметры для совместимости с oAuth-авторизацией (появится в Фотостране в обозримом будущем)
 define('FOTOSTRANA_OAUTH_CALLBACK', 'http://'.$_SERVER['HTTP_HOST'].'/sdk/callback-example.php');
 define('FOTOSTRANA_REQUIRED_PERMISSIONS', 'basic,friends');
+
+// параметры отладки и кэширования
 define('FOTOSTRANA_DEBUG', 0);
 define('FOTOSTRANA_REQUESTS_CACHE_TIMEOUT', 30);
 
+// в эти константы заносятся соответствующие значения из строки iframe при загрузке вашего приложения
 define('FOTOSTRANA_SESSION_KEY', $_REQUEST['sessionKey']);
 define('FOTOSTRANA_VIEWER_ID', $_REQUEST['viewerId']);
 
+// обменный курс для функций с валютой
 define('FOTOSTRANA_EXCHANGE', 1);
 
 // битовые маски для appSettings
-
 define('FOTOSTRANA_MASK_DEFAULT', 1);
 define('FOTOSTRANA_MASK_USERWALL', 2);
 define('FOTOSTRANA_MASK_USERCOMMUNITIES', 4);
